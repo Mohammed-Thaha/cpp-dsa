f115754b348bf5d25c07b2da0967f4753f02dff1
Mohammed-Thaha <mohammedthahacse@gmail.com>
2024-10-11 14:10:08 +0530

    Bitwise operator

---
+++ Bitwise operator/shift_operator.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void left_shift_operator(int a,int b){
+    cout<<(a<<b)<<endl;     //a left shift b
+    //This operation works like this a*2^b
+}
+void right_shift_operator(int a,int b){
+    cout<<(a>>b)<<endl;   //a right shift b
+    //This operator works like this a/(2^b)
+}
+int main(){
+    left_shift_operator(5,2); //5*(2^2)=20
+    left_shift_operator(6,3); //6*(2^3)=48
+    right_shift_operator(10,3);//10/(2^3)=1
+    right_shift_operator(32,6);//36/(2^6)=0
+    return 0;
+}
