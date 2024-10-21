03d59729cf2df338c79bb3778704a8fcac0c2582
Mohammed-Thaha <mohammedthahacse@gmail.com>
2024-10-21 20:01:07 +0530

    set bit in cpp

---
+++ Bitwise_operator/set_bit.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int count_set_bit(int n){
+    int cnt=0;
+    while(n>0){
+        cnt+=(n&1);
+        n=n>>1;
+    }
+    return cnt;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<count_set_bit(n);
+    return 0;
+} 
