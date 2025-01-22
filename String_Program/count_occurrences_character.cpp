b0f7923bf59ce18ca182252caab7ad120f36741e
technologygeeks <geeksc6@gmail.com>
2025-01-22 15:41:50 +0530

    string program

---
+++ String_Program/count_occurrences_character.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //In this program i am just counting the characters count in a string using array
+    string s;
+    cin>>s;
+    int n=s.size();
+    int arr[256]={0}; //All the Ascii character initialize with 0
+    for(char c: s){
+        arr[c]++;
+    }
+    for(int i=0;i<n;i++){
+        cout<<s[i]<<":"<<arr[s[i]]<<endl;
+    }
+    return 0;
+} 
+
